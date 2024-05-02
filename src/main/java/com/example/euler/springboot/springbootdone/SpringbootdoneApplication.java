5de0f477b485c2f5440c5f4b1059c3b359829f1a
DeepakEuler <deepaksharmaforwork@gmail.com>
2024-05-03 02:29:32 +0530

    initial commit

---
+++ src/main/java/com/example/euler/springboot/springbootdone/SpringbootdoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.euler.springboot.springbootdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdoneApplication.class, args);
+	}
+
+}
